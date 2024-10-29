b8daa290ef9fce7be98de3b53b75d94993beb377
yurii bilous <yurii.b@elogic.com.ua>
2024-10-29 22:32:38 +0100

    default components for page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const HandleClick1 = () => {
+    Alert.alert("Yuronn", "Hello, world!", [
+      { text: "OK" , onPress: () => console.log("OK Pressed")},
+      { text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel" },
+    ]);
+  };
+  const HandleClick2 = () => {
+    Alert.prompt("Yuronn", "Hello, world!", [
+      { text: "OK" , onPress: () => console.log("OK Pressed")},
+      { text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel" },
+    ]);
+  };
+
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Олюська топ!.</Text>
-    </View>
+      <Text>Default text</Text>
+      <Button title={"Press me1"} onPress={() => HandleClick1()} color={"red"} />
+      <Button title={"Press me2"} onPress={() => HandleClick2()} color={"red"} />
+    </SafeAreaView>
   );
 }
