ad80f87261f86d341be7daf13079213ad67538ae
yurii bilous <yurii.b@elogic.com.ua>
2024-10-31 22:56:17 +0100

    preparing for news app

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,9 @@
 import { Text, View, Button, Alert, FlatList } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Header from "../components/Header";
-import ListItem from "../components/ListItem";
-import Form from "../components/Form";
-import { useState } from "react";
+import {gStyle} from "../styles/style";
 
 export default function Index() {
-  const [listOfItems, setListOfItems] = useState([
-    { text: "Item 1", key: "1" },
-    { text: "Item 2", key: "2" },
-    { text: "Item 3", key: "3" },
-    { text: "Item 4", key: "4" },
-  ]);
+  return <View style={gStyle.main}>
 
-  const addHandler = (text) => {
-    setListOfItems((list) => {
-      return [{ text, key: Math.random().toString() }, ...list];
-    });
-  };
-
-  const deleteHandler = (key) => {
-    setListOfItems((list) => {
-      return list.filter((el) => el.key != key);
-    });
-  };
-
-  return (
-    <View>
-      <Header />
-      <Form addHandler={addHandler} />
-      <View>
-        <FlatList
-          data={listOfItems}
-          renderItem={({ item }) => (
-            <ListItem el={item} deleteHandler={deleteHandler} />
-          )}
-        />
-      </View>
-    </View>
-  );
+  </View>;
 }
+
---
+++ styles/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const gStyle = StyleSheet.create({
+    main: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "red"
+    },
+    text: {
+        fontSize: 24,
+        lineHeight: 32,
+        padding: 16,
+        color: "#0a7ea4",
+        borderBottomWidth: 1,
+        borderBottomColor: "#0a7ea4",
+        textAlign: "center",
+    },
+})
