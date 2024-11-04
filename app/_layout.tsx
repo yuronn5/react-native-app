807d094616ba655608af9fc6b97468db407afc8b
yurii bilous <yurii.b@elogic.com.ua>
2024-11-04 12:57:00 +0100

    created new screen (routing)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <>
-      <Text>Header</Text> 
-      <Slot />
-      <Text>Footer</Text>
-    </>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {Link} from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>StickerSmash</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}>Profile</Link>
     </View>
   );
 }
